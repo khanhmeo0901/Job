c615b7efc29b13d10c0f546734779fa7f3c528b7
Nguyễn Văn Khánh <khanh.nguyen01@lifesup.local>
2023-10-05 10:50:32 +0700

    add to create branch

--- src/main/java/org/example/ConvertDocxToPdf/TestConvert.java
+++ src/main/java/org/example/ConvertDocxToPdf/TestConvert.java
@@ -8,53 +8,19 @@ import java.io.*;
 import java.util.List;
 
 public class TestConvert {
-
-    // Check conflict repo : org.apache.xmlbeans.XmlOptions org.apache.xmlbeans.XmlOptions.setEntityExpansionLimit(int)
-
-
     public static void main(String[] args) {
         try (InputStream is = new FileInputStream(new File("C:\\Users\\ADMIN\\Desktop\\Công Việc\\Data test\\BA Nguyễn Thị Hương .docx"));
              OutputStream out = new FileOutputStream(new File("C:\\Users\\ADMIN\\Desktop\\Công Việc\\Data test\\test03102023.pdf"));) {
             long start = System.currentTimeMillis();
-            // 1) Load DOCX into XWPFDocument
             XWPFDocument document = new XWPFDocument(is);
-//            for (XWPFParagraph paragraph : document.getParagraphs()) {
-//                for (XWPFRun run : paragraph.getRuns()) {
-//                    if (run.isBold()) {
-//                        // Nếu từ hoặc đoạn văn bản này đã được đặt in đậm, tiếp tục chỉ đặt chữ in đậm
-//                        run.setBold(true);
-//                        run.setFontSize(10);
-//                    }
-//                }
-//            }
-//            for (XWPFTable table : document.getTables()) {
-//                List<XWPFTableRow> rows = table.getRows();
-//                for (XWPFTableRow row : rows) {
-//                    List<XWPFTableCell> cells = row.getTableCells();
-//                    for (XWPFTableCell cell : cells) {
-//                        for (XWPFParagraph paragraph : cell.getParagraphs()) {
-//                            for (XWPFRun run : paragraph.getRuns()) {
-//                                if (run.isBold()) {
-//                                    run.setBold(true);
-//                                    run.setFontSize(10);
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-
-            // Duyệt qua từng phần trong tài liệu
             List<IBodyElement> elements = document.getBodyElements();
             for (IBodyElement element : elements) {
                 if (element instanceof XWPFParagraph) {
-                    // Xử lý dòng văn bản
                     XWPFParagraph paragraph = (XWPFParagraph) element;
                     String text = paragraph.getText();
                     for (XWPFRun run : paragraph.getRuns()) {
                         if (run.isBold()) {
                             run.setBold(true);
-//                            run.setFontSize(12);
                             run.setFontFamily("Times New Roman");
                         }
                     }
@@ -69,7 +35,6 @@ public class TestConvert {
                                     for (XWPFRun run : cellParagraph.getRuns()) {
                                         if (run.isBold()) {
                                             run.setBold(true);
-//                                            run.setFontSize(10);
                                             run.setFontFamily("Times New Roman");
                                         }
                                     }
@@ -79,9 +44,7 @@ public class TestConvert {
                 }
             }
 
-            // 2) Prepare Pdf options
             PdfOptions options = PdfOptions.create();
-            // 3) Convert XWPFDocument to Pdf
             PdfConverter.getInstance().convert(document, out, options);
             System.out.println("Success :: " + (System.currentTimeMillis() - start) + " milli seconds");
         } catch (Throwable e) {
