fcfedef58ec69fd3a4592bce5d0c0b6229f79d9f
khanhmeo0901 <khanhvn09012000@gmail.com>
2024-01-05 17:03:27 +0700

    test3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String[] args) {
         test1();
         test2();
+        test3();
     }
     private static void test1() {
         System.out.println("Hello1");
     }
     private static void test2() {
         System.out.println("Hello2");
+    }
+    private static void test3() {
+        System.out.println("Hello3");
     }
 }
 
