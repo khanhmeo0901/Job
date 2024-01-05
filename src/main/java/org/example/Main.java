724b4123598512ceffa2017ec6189c845adb07e6
khanhmeo0901 <khanhvn09012000@gmail.com>
2024-01-05 17:02:42 +0700

    test2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         test1();
+        test2();
     }
     private static void test1() {
         System.out.println("Hello1");
     }
-
+    private static void test2() {
+        System.out.println("Hello2");
+    }
 }
 
 
