def0c8128febee07e60eff59a4f0f94ad26287e7
khanhmeo0901 <khanhvn09012000@gmail.com>
2024-01-05 16:35:58 +0700

    test git 1

--- src/main/java/org/example/Code/handleException/HandleException.java
+++
@@ -1,7 +0,0 @@
-package org.example.Code.handleException;
-
-public class HandleException extends RuntimeException {
-    public HandleException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/example/Code/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.Code.repo;
-
-import org.example.Code.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Custom query methods if needed
-}
